366e22ba8d4ae92cf8dce4cc97ea1f9a97e82842
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-02-13 18:49:34 -0300

    feat: New blog post page

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,28 @@
+import { client, getEntries } from "@/lib/contentful";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { notFound } from "next/navigation";
+import Codepen from "../Codepen";
+
+async function getBlogPost(slug: string) {
+  const blogPost = await client.getEntries({
+    content_type: "post",
+    "fields.slug": slug,
+  });
+  return blogPost;
+}
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const { slug } = params;
+  const query = await getBlogPost(slug);
+  if (!query.items[0]) {
+    notFound();
+  }
+  const blog = query.items[0].fields;
+  return (
+    <article>
+      <h1>{blog.title}</h1>
+      {documentToReactComponents(blog.content)}
+      <Codepen hash={blog.codepenHash} />
+    </article>
+  );
+}
---
+++ src/lib/contentful.ts
@@ -0,0 +1,20 @@
+import { createClient } from "contentful";
+
+if (!process.env.CONTENTFUL_SPACE_ID) {
+  throw new Error("Missing Contentful space id");
+}
+
+if (!process.env.CONTENTFUL_ACCESS_TOKEN) {
+  throw new Error("Missing Contentful access token");
+}
+
+const space_id = process.env.CONTENTFUL_SPACE_ID as string;
+const access_token = process.env.CONTENTFUL_ACCESS_TOKEN as string;
+
+export const client = createClient({
+  space: space_id,
+  accessToken: access_token,
+});
+
+export const getEntries = (entryType: string) =>
+  client.getEntries({ content_type: entryType });
