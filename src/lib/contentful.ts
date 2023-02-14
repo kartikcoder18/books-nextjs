c6fa2be7fa57775c85e4320e802274ecbac21472
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-02-14 20:56:48 -0300

    fix: remove extra function not used

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -6,12 +6,3 @@ export const getEntries = async (entryType: string) => {
   const result = queryScheme.parse(query);
   return result;
 };
-
-export const getEntryBySlug = async (slug: string, entryType: string) => {
-  const query = await client.getEntries({
-    content_type: entryType,
-    "fields.slug": slug,
-  });
-  const result = queryScheme.parse(query);
-  return result;
-};
