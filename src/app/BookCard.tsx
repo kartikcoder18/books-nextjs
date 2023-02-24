1749d96801332e41e68c96e45909fd1c72b62299
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-02-24 17:48:29 -0300

    feat: bookcard initial styling

--- src/app/BookCard.tsx
+++ src/app/BookCard.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
+import type { Book } from "@/lib/schemes";
+import "./bookcard.css";
 
-export default function BookCard({ props }) {
+export default function BookCard({ props }: { props: Book }) {
   return (
-    <>
+    <div className="book-card">
       <h2>{props.bookTitle}</h2>
-      <p>{props.author}</p>
-      <p>{props.status}</p>
-      <Image alt="" src={props.coverLink} width="300" height="460" />
-      {props.highlight}
-    </>
+      <p className="book-author">{props.author}</p>
+      <p className="book-details">
+        {props.publisher} ({props.publishedDate})
+      </p>
+      <div className="book-cover">
+        <Image alt="" src={props.coverLink} width="150" height="230" />
+      </div>
+    </div>
   );
 }
