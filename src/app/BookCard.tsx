f57ebb4d6b7b469e1ce6d5410623f18d0e600f85
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-03-04 11:03:54 -0300

    feat: new Badge component

--- src/app/BookCard.tsx
+++ src/app/BookCard.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import type { Book } from "@/lib/schemes";
 import "./bookcard.css";
+import Badge from "./Badge";
 
 export default function BookCard({ props }: { props: Book }) {
   return (
     <li className="book-card">
+      <div className="badge">
+        <Badge status={props.status} />
+      </div>
       <div className="book-info">
         <h3>{props.bookTitle}</h3>
         <p className="book-author">{props.author}</p>
