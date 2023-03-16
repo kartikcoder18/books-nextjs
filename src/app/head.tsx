a318a4b58a2550001f1581ce1ebfff0dff6190ec
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-03-16 18:22:39 -0300

    feat: OG and Twitter meta tags

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -8,6 +8,31 @@ export default function Head() {
         content="A list of all the books I'm reading, read or will read"
       />
       <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://ralacerda-books.netlify.app/" />
+      <meta property="og:title" content="Bookshelf" />
+      <meta
+        property="og:description"
+        content="Explore my curated book collection on my bookshelf website, built with Next.js and Contentful CMS. "
+      />
+      <meta
+        property="og:image"
+        content="https://ralacerda-books.netlify.app/meta.png"
+      />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:url"
+        content="https://ralacerda-books.netlify.app/"
+      />
+      <meta property="twitter:title" content="Bookshelf" />
+      <meta
+        property="twitter:description"
+        content="Explore my curated book collection on my bookshelf website, built with Next.js and Contentful CMS. "
+      />
+      <meta
+        property="twitter:image"
+        content="https://ralacerda-books.netlify.app/meta.png"
+      />
     </>
   );
 }
