2ee68bba54bdee29caf76322a422e6a0f6184e5d
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-03-16 17:06:47 -0300

    fix: name and favicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,13 +1,13 @@
 export default function Head() {
   return (
     <>
-      <title>Books</title>
+      <title>Bookshelf</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
         name="description"
         content="A list of all the books I'm reading, read or will read"
       />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/icon.svg" type="image/svg+xml" />
     </>
   );
 }
