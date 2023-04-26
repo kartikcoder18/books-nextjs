2d3878b572511feb39b2369ffcd277e3c4716d8d
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-04-26 11:36:26 -0300

    fix: technical books first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
 
   return (
     <main>
-      {nonFictionList} {fictionList} {technicalList}
+      {technicalList} {nonFictionList} {fictionList}
     </main>
   );
 }
